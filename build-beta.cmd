75a6005c313804c896979dc7ff4a3756e7deda69
NagiLin <johnny5581@gmail.com>
2018-01-03 17:03:29 +0800

    增加BuildScript，修正Insert、Update時使用Hardword，修正Between兼容Value.Between(Field, Field)之狀況

--- build-beta.cmd
+++ build-beta.cmd
@@ -1,6 +1,5 @@
-@echo off
+@ECHO OFF
 SET /P VER=Enter the version [1]: 
-IF /I "%VER%" EQU "" set "ver=1"
-nuget pack src\Ancestor\Ancestor.Core\Ancestor.Core.csproj -Suffix Beta-%VER% -OutputDirectory build\nupkgs\ -Build
-nuget pack src\Ancestor\Ancestor.DataAccess\Ancestor.DataAccess.csproj -Suffix Beta-%VER% -OutputDirectory build\nupkgs\ -Build
+IF /I "%VER%" EQU "" SET "ver=1"
+nuget pack src\Ancestor\Ancestor.Core\Ancestor.Core.csproj -OutputDirectory build\nupkgs\ -Build -Properties Configuration=Release;Beta=%VER%
 pause
