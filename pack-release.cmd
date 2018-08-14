4f52dd3730c8d3afc27c99bf9e56ac2801a48202
NagiLin <johnny5581@gmail.com>
2018-08-14 12:08:53 +0800

    Restructer project
    Move Ancestor.* to Parent folder, Remove Ancestor folder.
    Modify Ancestor.DataAccess folder structer.
    Remove build script.
    Fix nuspec.
    Fix reference packages.

--- pack-release.cmd
+++
@@ -1,4 +0,0 @@
-@echo off
-nuget pack src\Ancestor\Ancestor.Core\Ancestor.Core.csproj -OutputDirectory build\nupkgs\  -Properties Configuration=Release
-nuget pack src\Ancestor\Ancestor.DataAccess\Ancestor.DataAccess.csproj -OutputDirectory build\nupkgs\   -Properties Configuration=Release
-pause
