ef1f5a3b2a31f76671d7c168a3812ded3590540b
Nagi Lin <johnny5581@gmail.com>
2019-04-10 15:53:59 +0800

    查詢補上BindByName
    - 推版本到1.3.11.0

--- src/Ancestor.Core/Properties/AssemblyInfo.cs
+++ src/Ancestor.Core/Properties/AssemblyInfo.cs
@@ -33,5 +33,5 @@ using System.Runtime.InteropServices;
 // 指定為預設值: 
 
 
-[assembly: AssemblyVersion("1.3.10.0")]
-[assembly: AssemblyInformationalVersion("1.3.10.0")]
+[assembly: AssemblyVersion("1.3.11.0")]
+[assembly: AssemblyInformationalVersion("1.3.11.0")]
