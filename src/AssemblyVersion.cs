a739dd82cd10561ecc2eb91159656abc35f4f15f
Nagi Lin <johnny5581@gmail.com>
2019-11-27 17:44:50 +0800

    改為使用Version.cs

---
+++ src/AssemblyVersion.cs
@@ -0,0 +1,9 @@
+using System.Reflection;
+
+#if DEBUG
+[assembly: AssemblyVersion("1.4.5.0")]
+[assembly: AssemblyInformationalVersion("1.4.5-beta2-mps10")]
+#else
+[assembly: AssemblyVersion("1.4.5.0")]
+[assembly: AssemblyInformationalVersion("1.4.5.0")]
+#endif
