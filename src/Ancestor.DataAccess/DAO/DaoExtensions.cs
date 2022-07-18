6403f024e3cf48239838a08851c4bda21c24da96
Nagi Lin <nagilin@cgmh.org.tw>
2022-07-18 10:36:46 +0800

    fix default direct connection string builder for Service_Name to SID (Oracle)

---
+++ src/Ancestor.DataAccess/DAO/DaoExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ancestor.DataAccess.DAO
+{
+    public static class DaoExtensions
+    {
+        public static void SetAutoClosed(this IDataAccessObject dataAccessObject, bool enabled)
+        {
+            var dao = dataAccessObject as DataAccessObject;
+            if (dao == null) throw new InvalidOperationException("instance is not DataAccessObject");
+            var db = dao.DB as DBAction.BaseAbstractAction;
+            if (db == null) throw new InvalidOperationException("instance's context is not DbAction");
+            db.AutoClosed = enabled;
+        }
+        public static void SetValidatable(this IDataAccessObject dataAccessObject, bool enabled)
+        {
+            var dao = dataAccessObject as DataAccessObject;
+            if (dao == null) throw new InvalidOperationException("instance is not DataAccessObject");
+            var db = dao.DB as DBAction.BaseAbstractAction;
+            if (db == null) throw new InvalidOperationException("instance's context is not DbAction");
+            db.Validatable = enabled;
+        }
+        public static void CloseConnection(this IDataAccessObject dataAccessObject)
+        {
+            var dao = dataAccessObject as DataAccessObject;
+            if (dao == null) throw new InvalidOperationException("instance is not DataAccessObject");
+            dao.DB.DBConnection.Close();
+        }
+    }
+}
--- src/AssemblyVersion.cs
+++ src/AssemblyVersion.cs
@@ -1,9 +1,4 @@
 using System.Reflection;
 
-#if DEBUG
-[assembly: AssemblyVersion("1.4.13.2")]
-[assembly: AssemblyInformationalVersion("1.4.13-b20211001-01")]
-#else
-[assembly: AssemblyVersion("1.4.14.5")]
-[assembly: AssemblyInformationalVersion("1.4.14.5")]
-#endif
+[assembly: AssemblyVersion("1.4.15.0")]
+[assembly: AssemblyInformationalVersion("1.4.15.0")]
